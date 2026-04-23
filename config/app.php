7d5d4ca75766701a14133388a4ff6fd27709ea3f
Christineart <e88292774@gmail.com>
2026-04-23 15:07:31 +0800

    fix config/app.php - remove corrupted scheduler_secret entry

--- config/app.php
+++ config/app.php
@@ -2,11 +2,16 @@
 
 return [
 
-    'scheduler_secret' => env('SCHEDULER_SECRET'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
+    |--------------------------------------------------------------------------
+    |
+    | This value is the name of your application, which will be used when the
+    | framework needs to place the application's name in a notification or
+    | other UI elements where an application name needs to be displayed.
+    |
+    */
 
     'name' => env('APP_NAME', 'Laravel'),
 
